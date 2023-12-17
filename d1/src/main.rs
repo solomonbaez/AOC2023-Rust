cfb8a928e4047374e09114f0d43bcd0582c31641
solomonbaez <solomon.james.baez@gmail.com>
2023-12-17 00:18:53 -0800

    Adjusting approach -> using a find-and-replace scheme -> odd entropy in system

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -6,68 +6,55 @@ fn main() {
     let data = File::open("src/data.txt").unwrap();
     let reader = BufReader::new(data);
 
-    let mut calibration_value: i32 = 0;
-    reader
-        .lines()
-        .for_each(|line| calibration_value += parse_line(line.unwrap()));
-
-    println!("calibration: {}", calibration_value);
-}
-
-fn parse_line(line: String) -> i32 {
-    let digits: HashMap<&str, i32> = HashMap::from([
-        ("zero", 0),
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
+    let digits: HashMap<&str, &str> = HashMap::from([
+        ("0", "zero"),
+        ("1", "one"),
+        ("2", "two"),
+        ("3", "three"),
+        ("4", "four"),
+        ("5", "five"),
+        ("6", "six"),
+        ("7", "seven"),
+        ("8", "eight"),
+        ("9", "nine"),
     ]);
 
-    let mut l = 0;
-    let mut r = 0;
+    let mut reader_lines: Vec<String> = Vec::new();
+    reader
+        .lines()
+        .for_each(|line| reader_lines.push(line.unwrap()));
 
-    //initialize first and last counter
-    let mut first = 0;
-    let mut second = 0;
-
-    //convert string to a vector representation
-    let line_characters: Vec<char> = line.chars().collect();
-    while r < line.len() {
-        if let Some(digit) = digits.get(&line[l..r + 1]) {
-            // println!("{digit}");
-            if first == 0 {
-                first = *digit;
-            } else {
-                second = *digit;
+    let mut find_and_replace: Vec<String> = Vec::new();
+    let _ = &reader_lines.iter_mut().for_each(|line| {
+        digits.iter().for_each(|(k, v)| {
+            if line.contains(*v) {
+                *line = line.replace(v, k);
             }
+        });
+        find_and_replace.push(line.to_string());
+    });
 
-            //shift left pointer
-            l = r + 1;
-        } else if line_characters[r].is_numeric() {
-            let digit = line_characters[r].to_digit(10).unwrap();
-            // println!("{digit}");
-
-            if first == 0 {
-                first = digit as i32;
-            } else {
-                second = digit as i32;
+    let mut calibration_value: i32 = 0;
+    println!("{:?}, {}", find_and_replace, find_and_replace.len());
+    find_and_replace.iter().for_each(|line| {
+        let mut first = 0;
+        let mut second = 0;
+        for c in line.chars() {
+            if c.is_numeric() {
+                if first == 0 {
+                    first = c.to_digit(10).unwrap() as i32;
+                } else {
+                    second = c.to_digit(10).unwrap() as i32;
+                }
             }
-
-            l = r + 1;
         }
 
-        r += 1;
-    }
+        if second != 0 {
+            calibration_value += (first * 10) + second;
+        } else {
+            calibration_value += first;
+        }
+    });
 
-    // println!("{}, {}", first, second);
-    if second != 0 {
-        first * 10 + second
-    } else {
-        first
-    }
+    println!("calibration: {}", calibration_value);
 }
