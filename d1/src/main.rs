51ac712f7a4478cf85972220c9ddd98675806d66
solomonbaez <solomon.james.baez@gmail.com>
2023-12-16 22:48:07 -0800

    Second pass -> somewhat messy parser, still not a complete solution as the sliding window implementation is flawed

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -1,37 +1,73 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
     let data = File::open("src/data.txt").unwrap();
-
-    // read by line to extract calibration keys
     let reader = BufReader::new(data);
 
     let mut calibration_value: i32 = 0;
-    let mut num_lines: i32 = 0;
-    for line in reader.lines() {
-        num_lines += 1;
-        let mut line_calibration = vec![-1; 2];
-        for s in line.unwrap().chars() {
-            if let Some(c) = s.to_digit(10) {
-                if line_calibration[0] == -1 {
-                    line_calibration[0] = c as i32;
-                } else {
-                    line_calibration[1] = c as i32;
-                }
+    reader
+        .lines()
+        .for_each(|line| calibration_value += parse_line(line.unwrap()));
+
+    println!("calibration: {}", calibration_value);
+}
+
+fn parse_line(line: String) -> i32 {
+    let digits: HashMap<&str, i32> = HashMap::from([
+        ("zero", 0),
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let mut l = 0;
+    let mut r = 0;
+
+    //initialize first and last counter
+    let mut first = 0;
+    let mut second = 0;
+
+    //convert string to a vector representation
+    let line_characters: Vec<char> = line.chars().collect();
+    while r < line.len() {
+        if let Some(digit) = digits.get(&line[l..r + 1]) {
+            // println!("{digit}");
+            if first == 0 {
+                first = *digit;
+            } else {
+                second = *digit;
             }
+
+            //shift left pointer
+            l = r + 1;
+        } else if line_characters[r].is_numeric() {
+            let digit = line_characters[r].to_digit(10).unwrap();
+            // println!("{digit}");
+
+            if first == 0 {
+                first = digit as i32;
+            } else {
+                second = digit as i32;
+            }
+
+            l = r + 1;
         }
 
-        let mut line_calibration_collect = 0;
-        if line_calibration[1] != -1 {
-            line_calibration_collect += line_calibration[0] * 10 + line_calibration[1];
-        } else {
-            line_calibration_collect += line_calibration[0];
-        }
-
-        // println!("{}", line_calibration_collect);
-        calibration_value += line_calibration_collect;
+        r += 1;
     }
 
-    println!("calibration: {}, in {} lines", calibration_value, num_lines);
+    // println!("{}, {}", first, second);
+    if second != 0 {
+        first * 10 + second
+    } else {
+        first
+    }
 }
