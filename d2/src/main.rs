58db5f0f2f7a2f83e38054a403d77baa83fde506
solomonbaez <solomon.james.baez@gmail.com>
2023-12-20 01:22:14 -0800

    Finished naive implementation with a dirty nested if/else block

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -7,6 +7,8 @@ use std::io::{BufRead, BufReader};
 // if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
 // What is the sum of the IDs of those  games?
 //
+static BAG: &'static [(&'static char, usize)] = &[(&'r', 12), (&'g', 13), (&'b', 14)];
+
 fn main() {
     let file = File::open("src/data.txt").unwrap();
     let data: Vec<String> = BufReader::new(file)
@@ -24,7 +26,7 @@ fn main() {
 
     let mut count = 0;
     games.iter().for_each(|game| {
-        if let Some(id) = is_valid(game) {
+        if let Some(id) = is_valid(game.to_vec()) {
             count += id;
         }
     });
@@ -32,7 +34,37 @@ fn main() {
     println!("{}", count);
 }
 
-fn is_valid(game: &Vec<&str>) -> Option<usize> {
-    println!("{:?}", game);
-    Some(1)
+fn is_valid(game: Vec<&str>) -> Option<usize> {
+    let mut id = 0;
+    let mut value = 0;
+    for line in game.iter() {
+        for c in line.chars() {
+            if c.is_numeric() && id == 0 {
+                id = c.to_digit(10).unwrap() as usize;
+            } else if c.is_numeric() && value == 0 {
+                value = c.to_digit(10).unwrap() as usize;
+            } else if c == 'r' {
+                if value > BAG[0].1 {
+                    return None;
+                }
+
+                value = 0
+            } else if c == 'g' {
+                if value > BAG[1].1 {
+                    return None;
+                }
+
+                value = 0
+            } else if c == 'b' {
+                if value > BAG[2].1 {
+                    return None;
+                }
+
+                value = 0;
+            }
+        }
+    }
+
+    // println!("{:?}", game);
+    Some(id)
 }
