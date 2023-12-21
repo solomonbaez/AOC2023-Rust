6849f5b7bfb4163f81fca540e3a863bd9516881b
solomonbaez <solomon.james.baez@gmail.com>
2023-12-20 21:10:37 -0800

    Messy but functional solution for day 2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -6,8 +6,12 @@ use std::io::{BufRead, BufReader};
 // question: determine which games would have been possible
 // if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
 // What is the sum of the IDs of those  games?
-//
-static BAG: &'static [(&'static char, usize)] = &[(&'r', 12), (&'g', 13), (&'b', 14)];
+
+enum Bag {
+    R = 12,
+    G = 13,
+    B = 14,
+}
 
 fn main() {
     let file = File::open("src/data.txt").unwrap();
@@ -24,6 +28,8 @@ fn main() {
         })
         .collect();
 
+    // println!("{:?}", games);
+
     let mut count = 0;
     games.iter().for_each(|game| {
         if let Some(id) = is_valid(game.to_vec()) {
@@ -34,37 +40,61 @@ fn main() {
     println!("{}", count);
 }
 
-fn is_valid(game: Vec<&str>) -> Option<usize> {
-    let mut id = 0;
-    let mut value = 0;
+fn is_valid(mut game: Vec<&str>) -> Option<i32> {
+    let split: Vec<&str> = game[0].split(":").collect();
+    game[0] = split[1];
+
+    let mut id_vec: Vec<u32> = Vec::new();
+    for c in split[0].chars().rev() {
+        if c.is_numeric() {
+            id_vec.push(c.to_digit(10).unwrap());
+        }
+    }
+
+    let mut value = 0 as i32;
     for line in game.iter() {
         for c in line.chars() {
-            if c.is_numeric() && id == 0 {
-                id = c.to_digit(10).unwrap() as usize;
-            } else if c.is_numeric() && value == 0 {
-                value = c.to_digit(10).unwrap() as usize;
+            if c.is_numeric() {
+                let digit = c.to_digit(10).unwrap() as i32;
+                if value != 0 {
+                    value = value * 10 + digit;
+                } else {
+                    value += digit;
+                }
+                println!("{c}, {value}");
             } else if c == 'r' {
-                if value > BAG[0].1 {
+                if value > Bag::R as i32 {
+                    println!("Wrong!");
                     return None;
+                } else {
+                    value = 0;
                 }
-
-                value = 0
             } else if c == 'g' {
-                if value > BAG[1].1 {
+                if value > Bag::G as i32 {
+                    println!("Wrong!");
                     return None;
+                } else {
+                    value = 0;
                 }
-
-                value = 0
             } else if c == 'b' {
-                if value > BAG[2].1 {
+                if value > Bag::B as i32 {
+                    println!("Wrong!");
                     return None;
+                } else {
+                    value = 0;
                 }
-
-                value = 0;
             }
         }
     }
 
     // println!("{:?}", game);
-    Some(id)
+    Some({
+        if id_vec.len() == 3 {
+            100
+        } else if id_vec.len() == 2 {
+            (id_vec[1] * 10 + id_vec[0]) as i32
+        } else {
+            id_vec[0] as i32
+        }
+    })
 }
