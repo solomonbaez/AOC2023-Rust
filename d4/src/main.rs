c0ffc886dae3ddc15c8d7c296cda42a2dfa4fd51
solomonbaez <solomon.james.baez@gmail.com>
2023-12-27 01:15:00 -0800

    Finished naive implementation -> assuming the current answer is too high due to counting duplicate matches in a card-set

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -9,17 +9,40 @@ fn main() {
         .collect();
 
     let cards: Vec<Vec<&str>> = data.iter().map(|line| line.split("|").collect()).collect();
-    println!("{:?}", cards[0]);
-    count_matches(&cards[0]);
+    let total: i32 = cards.iter().map(|card| count_matches(card)).sum();
+
+    println!("{}", total)
 }
 
 fn count_matches(card: &Vec<&str>) -> i32 {
     let targets: Vec<&str> = clean_card((card[0].split(":").collect::<Vec<&str>>())[1]);
     let numbers: Vec<&str> = clean_card(card[1]);
 
-    println!("targets: {:?}, numbers: {:?}", targets, numbers);
+    // println!("targets: {:?}, numbers: {:?}", targets, numbers);
 
-    return 0;
+    let sum: i32 = numbers
+        .iter()
+        .map(|number| {
+            if targets.contains(number) {
+                let mut delta = 1;
+                let sub_sum: i32 = number
+                    .chars()
+                    .rev()
+                    .map(|digit| {
+                        let base_10 = (digit.to_digit(10).unwrap() as i32) * delta;
+                        delta *= 10;
+                        base_10
+                    })
+                    .sum::<i32>();
+                sub_sum
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    // println!("{:?}", sum);
+    sum //TODO!() CURRENTLY TOO HIGH -> could be duplicate matches
 }
 
 fn clean_card(card: &str) -> Vec<&str> {
