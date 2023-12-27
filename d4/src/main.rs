bfc3ae2262df6bae5187a786c097afd13741e53e
solomonbaez <solomon.james.baez@gmail.com>
2023-12-27 00:56:14 -0800

    Added card cleaning logic with a helper function

--- d4/src/main.rs
+++ d4/src/main.rs
@@ -10,4 +10,20 @@ fn main() {
 
     let cards: Vec<Vec<&str>> = data.iter().map(|line| line.split("|").collect()).collect();
     println!("{:?}", cards[0]);
+    count_matches(&cards[0]);
+}
+
+fn count_matches(card: &Vec<&str>) -> i32 {
+    let targets: Vec<&str> = clean_card((card[0].split(":").collect::<Vec<&str>>())[1]);
+    let numbers: Vec<&str> = clean_card(card[1]);
+
+    println!("targets: {:?}, numbers: {:?}", targets, numbers);
+
+    return 0;
+}
+
+fn clean_card(card: &str) -> Vec<&str> {
+    card.split(" ")
+        .filter(|number| !number.is_empty())
+        .collect()
 }
