0d34cd5f90a9d94a2770f344f6f52d8c57ddf42e
solomonbaez <solomon.james.baez@gmail.com>
2023-12-30 23:55:57 -0800

    Attempting to DRY up d1 code

--- d1/src/main.rs
+++ d1/src/main.rs
@@ -12,27 +12,35 @@ fn main() {
 
     let calibration_value: i32 = lines.iter().map(|line| parse_line(line.to_string())).sum();
 
+    // ANSWER==54265
     println!("calibration: {}", calibration_value);
 }
 
 fn parse_line(line: String) -> i32 {
+    let line_chars: Vec<char> = line.chars().collect();
     let mut first = -1;
     let mut second = -1;
 
-    let line_chars: Vec<char> = line.chars().collect();
-    for i in 0..line.len() {
-        if line_chars[i].is_numeric() {
-            first = line_chars[i].to_digit(10).unwrap() as i32;
-            break;
-        } else {
-            // println!("{:?}", &line[0..i]);
-            for (number, digit) in DIGITS.iter().enumerate() {
-                if line[0..i + 1].contains(digit) {
-                    first = (number + 1) as i32;
-                    // println!("{first}");
-                    break;
+    let get_keys = |i: usize, reverse: bool| -> Option<i32> {
+        match line_chars[i].is_numeric() {
+            true => Some(line_chars[i].to_digit(10).unwrap() as i32),
+            false => {
+                for (number, digit) in DIGITS.iter().enumerate() {
+                    if reverse && line[0..i + 1].contains(digit) {
+                        Some((number + 1) as i32)
+                    } else if {
+                        continue;
+                    };
                 }
+
+                None
             }
+        }
+    };
+
+    for i in 0..line.len() {
+        if let Some(key) = get_keys(i, false) {
+            first = key;
         }
 
         if first != -1 {
@@ -41,18 +49,8 @@ fn parse_line(line: String) -> i32 {
     }
 
     for j in (0..line.len()).rev() {
-        if line_chars[j].is_numeric() {
-            second = line_chars[j].to_digit(10).unwrap() as i32;
-            break;
-        } else {
-            // println!("{:?}", &line[j..line.len()]);
-            for (number, digit) in DIGITS.iter().enumerate() {
-                if line[j..line.len()].contains(digit) {
-                    second = (number + 1) as i32;
-                    // println!("{second}");
-                    break;
-                }
-            }
+        if let Some(key) = get_keys(j, true) {
+            second = key;
         }
 
         if second != -1 {
